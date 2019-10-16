96bb45e817de0cbe435876ef7ea63eebe3bbb6bd
Alexey Khrushch <alexeykhr@outlook.com>
2019-10-16 21:13:14 +0300

    chore(package): add dotenv + .env

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 'use strict'
 
 const packageJson = require('./package')
+require('dotenv').config()
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? `/${packageJson.name}/`
-    : '/'
+    ? process.env.APP_BUILD_URL || `/${packageJson.name}/`
+    : process.env.APP_BUILD_URL || '/'
 }
