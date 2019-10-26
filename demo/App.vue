236e7d76c77a4e032969b19ae1ea57c67c2fd336
Alexey Khrushch <alexeykhr@outlook.com>
2019-10-26 14:27:09 +0300

    chore(docs): fill for vsm-mob

--- demo/App.vue
+++ demo/App.vue
@@ -4,10 +4,13 @@
     <main>
       <install-docs />
       <example-docs />
-      <attributes-docs />
-      <menu-object-docs />
-      <slots-docs />
-      <events-docs />
+      <props-menu-docs />
+      <menu-object-menu-docs />
+      <slots-menu-docs />
+      <events-menu-docs />
+      <props-mob-docs />
+      <slots-mob-docs />
+      <classes-docs />
       <demo-docs />
     </main>
     <base-footer />
@@ -15,26 +18,32 @@
 </template>
 
 <script>
-import AttributesDocs from './components/docs/Attributes'
-import MenuObjectDocs from './components/docs/MenuObject'
+import MenuObjectMenuDocs from './components/docs/menu/MenuObject'
+import EventsMenuDocs from './components/docs/menu/Events'
+import PropsMenuDocs from './components/docs/menu/Props'
+import SlotsMenuDocs from './components/docs/menu/Slots'
+import PropsMobDocs from './components/docs/mob/Props'
+import SlotsMobDocs from './components/docs/mob/Slots'
+import ClassesDocs from './components/docs/Classes'
 import ExampleDocs from './components/docs/Example'
 import InstallDocs from './components/docs/Install'
-import EventsDocs from './components/docs/Events'
 import BaseHeader from './components/BaseHeader'
 import BaseFooter from './components/BaseFooter'
-import SlotsDocs from './components/docs/Slots'
 import DemoDocs from './components/docs/Demo'
 
 export default {
   components: {
-    AttributesDocs,
-    MenuObjectDocs,
+    MenuObjectMenuDocs,
+    EventsMenuDocs,
+    PropsMenuDocs,
+    SlotsMenuDocs,
+    PropsMobDocs,
+    SlotsMobDocs,
+    ClassesDocs,
     ExampleDocs,
     InstallDocs,
     BaseHeader,
-    EventsDocs,
     BaseFooter,
-    SlotsDocs,
     DemoDocs
   }
 }
