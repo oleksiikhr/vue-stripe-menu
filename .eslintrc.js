b07b33d7c94e056f442bdf2a1ecba15dbd8d7d55
Alexey Khrushch <alexeykhr@outlook.com>
2020-12-01 00:28:05 +0200

    feat: upgrade libraries to vue3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,9 @@
 module.exports = {
   extends: [
-    '@vue/standard',
     'plugin:vue/base',
-    'plugin:vue/essential',
-    'plugin:vue/recommended',
-    'plugin:vue/strongly-recommended'
+    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
+    'plugin:vue/vue3-strongly-recommended'
   ],
   parserOptions: {
     parser: 'babel-eslint'
