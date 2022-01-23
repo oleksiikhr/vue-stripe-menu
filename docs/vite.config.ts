3e8ee0fe1c5581e698993ecc4dfab8bc9017ee8b
Alexey Khrushch <alexeykhr@outlook.com>
2022-01-23 19:55:09 +0200

    chore: remove default base for docs

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue';
 
 const config: UserConfig = {
   plugins: [vue()],
-  base: 'production' === process.env.NODE_ENV ? '/vue-stripe-menu/' : '',
 };
 
 export default config;
